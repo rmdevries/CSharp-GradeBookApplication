131501c9fe8e947a6fcbf88a0f0921e2c070a6bc
Robert de Vries <robert.devries@coolblue.nl>
2018-09-03 16:34:21 +0200

    Completed the module2

---
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradebook : BaseGradeBook
+    {
+        public StandardGradebook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
